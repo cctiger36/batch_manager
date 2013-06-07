1444e674a5995d119943bae038cecbac659a5fe2
Weihu Chen <cctiger36@gmail.com>
2013-06-07 20:50:58 +0900

    fix bug

--- app/controllers/batch_manager/batches_controller.rb
+++ app/controllers/batch_manager/batches_controller.rb
@@ -1,15 +1,15 @@
 module BatchManager
   class BatchesController < ApplicationController
+    before_filter :retain_batch_params, :except => [:index]
+
     def index
       @details = BatchManager::Monitor.details
     end
 
     def exec
-      @batch_name = params[:batch_name]
-      @wet = params[:wet]
       if resque_supported?
         Resque.enqueue(BatchManager::ExecBatchWorker, @batch_name, :wet => @wet)
-        @offset = log_file.size
+        redirect_to(log_batches_url(:batch_name => @batch_name, :wet => @wet))
       else
         BatchManager::Executor.exec(@batch_name, :wet => @wet)
         redirect_to(batches_url)
@@ -17,17 +17,21 @@ module BatchManager
     end
 
     def async_read_log
-      file = log_file
-      file.seek(params[:offset].to_i) if params[:offset].present?
-      render :json => {:content => file.read, :offset => file.size}
+      log_file.seek(params[:offset].to_i) if params[:offset].present?
+      render :json => {:content => log_file.read, :offset => log_file.size}
     end
 
     def log
-      log_file = BatchManager::Logger.log_file_path(params[:batch_name], params[:wet])
-      render :file => log_file, :content_type => Mime::TEXT, :layout => false
+      @offset = log_file.size
+      @content = log_file.read
     end
 
     private
+
+    def retain_batch_params
+      @batch_name = params[:batch_name]
+      @wet = params[:wet]
+    end
 
     def resque_supported?
       begin
@@ -39,7 +43,15 @@ module BatchManager
     end
 
     def log_file
-      File.open(BatchManager::Logger.log_file_path(params[:batch_name], params[:wet]), 'r')
+      20.times do
+        begin
+          @log_file ||= File.open(BatchManager::Logger.log_file_path(@batch_name, @wet), 'r')
+          return @log_file
+        rescue
+          sleep 3
+          retry
+        end
+      end
     end
   end
 end
