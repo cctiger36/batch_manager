7d76082bda55b79c1a6b420a5e5792889cd93ed2
Weihu Chen <cctiger36@gmail.com>
2013-06-14 17:22:55 +0900

    refactor

--- lib/batch_manager/executor.rb
+++ lib/batch_manager/executor.rb
@@ -4,11 +4,12 @@ module BatchManager
 
     class << self
       def exec(batch_file, options = {})
-        batch_file_path = batch_full_path(batch_file)
-        if File.exist?(batch_file_path)
-          batch_status = BatchManager::BatchStatus.new(batch_file_path)
-          if options[:force] || !options[:wet] || batch_status.can_run?
-            exec_batch_script(batch_file_path, batch_status, options[:wet])
+        @batch_file_path = batch_full_path(batch_file)
+        if File.exist?(@batch_file_path)
+          @batch_status = BatchManager::BatchStatus.new(@batch_file_path)
+          @wet = options[:wet]
+          if options[:force] || !@wet || @batch_status.can_run?
+            record_run_duration { exec_batch_script }
           else
             raise "Cannot run this batch."
           end
@@ -19,29 +20,32 @@ module BatchManager
 
       protected
 
-      def exec_batch_script(batch_file_path, batch_status, is_wet)
-        logger = BatchManager::Logger.new(batch_status.name, is_wet)
-        write_log_header(is_wet)
-        @wet = is_wet
+      def record_run_duration
+        @logger = BatchManager::Logger.new(@batch_status.name, @wet)
         start_at = Time.now
-        eval(File.read(batch_file_path))
-        logger.info "Succeeded."
-        batch_status.update_schema if is_wet
-      rescue => e
-        logger.error e
-        logger.info "Failed."
-      ensure
+        yield
         end_at = Time.now
-        logger.info "End at: #{end_at.strftime("%Y-%m-%d %H:%M:%S")} (#{(end_at - start_at).to_i}s)"
-        puts "Log saved at: #{BatchManager.logger.log_file}" if logger.log_file
-        logger.close
+        @logger.info "End at: #{end_at.strftime("%Y-%m-%d %H:%M:%S")} (#{(end_at - start_at).to_i}s)"
+        @logger.close
       end
 
-      def write_log_header(is_wet)
-        BatchManager.logger.info "=============================="
-        BatchManager.logger.info "= #{is_wet ? 'WET' : 'DRY'} RUN"
-        BatchManager.logger.info "= Ran at: #{Time.now.strftime("%Y-%m-%d %H:%M:%S")}"
-        BatchManager.logger.info "=============================="
+      def exec_batch_script
+        write_log_header
+        eval(File.read(@batch_file_path))
+        @logger.info "Succeeded."
+        @batch_status.update_schema if @wet
+      rescue => e
+        @logger.error e
+        @logger.info "Failed."
+      ensure
+        puts "Log saved at: #{@logger.log_file}" if @logger.log_file
+      end
+
+      def write_log_header
+        @logger.info "=============================="
+        @logger.info "= #{@wet ? 'WET' : 'DRY'} RUN"
+        @logger.info "= Ran at: #{Time.now.strftime("%Y-%m-%d %H:%M:%S")}"
+        @logger.info "=============================="
       end
     end
   end
