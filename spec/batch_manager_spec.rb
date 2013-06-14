934af1993b4b36df83b6e95e25fa326a4c57676c
Weihu Chen <cctiger36@gmail.com>
2013-06-14 18:16:55 +0900

    fix spec desc

--- spec/batch_manager_spec.rb
+++ spec/batch_manager_spec.rb
@@ -6,8 +6,10 @@ describe BatchManager do
     BatchManager.batch_dir.should == temp_dir
   end
 
-  it "should be a Logger as default" do
-    BatchManager.logger.should be_an_instance_of(::Logger)
+  describe "::logger" do
+    it "should be a Logger as default" do
+      BatchManager.logger.should be_an_instance_of(::Logger)
+    end
   end
 
 end
