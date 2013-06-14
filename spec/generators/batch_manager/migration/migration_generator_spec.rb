307267c585e747bb6ca74d431e964c8a7deb6e76
Weihu Chen <cctiger36@gmail.com>
2013-06-14 14:48:30 +0900

    add spec

--- spec/generators/batch_manager/migration/migration_generator_spec.rb
+++ spec/generators/batch_manager/migration/migration_generator_spec.rb
@@ -4,17 +4,35 @@ require 'generators/batch_manager/migration/migration_generator'
 describe BatchManager::MigrationGenerator do
   destination temp_dir
 
-  before do
-    prepare_destination
-    Rails::Generators.options[:rails][:orm] = :active_record
+  context "when active_record" do
+    before do
+      prepare_destination
+      Rails::Generators.options[:rails][:orm] = :active_record
+    end
+
+    describe 'the generated files' do
+      before { run_generator }
+
+      describe 'db/migrate/batch_manager_migration.rb' do
+        subject { file('db/migrate/batch_manager_migration.rb') }
+        it { should be_a_migration }
+      end
+    end
   end
 
-  describe 'the generated files' do
-    before { run_generator }
+  context "when orm hasn't migration" do
+    before do
+      prepare_destination
+      Rails::Generators.options[:rails][:orm] = :mongoid
+    end
 
-    describe 'db/migrate/batch_manager_migration.rb' do
-      subject { file('db/migrate/batch_manager_migration.rb') }
-      it { should be_a_migration }
+    describe 'the generated files' do
+      before { run_generator }
+
+      describe 'db/migrate/batch_manager_migration.rb' do
+        subject { file('db/migrate/batch_manager_migration.rb') }
+        it { should_not exist }
+      end
     end
   end
 end
