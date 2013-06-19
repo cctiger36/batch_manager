4ca6c282abeb5d2f78dfd60ae8f64fa009c9f153
Weihu Chen <cctiger36@gmail.com>
2013-06-19 21:46:25 +0900

    add precompile assets config

--- lib/batch_manager/engine.rb
+++ lib/batch_manager/engine.rb
@@ -10,5 +10,9 @@ module BatchManager
     config.batch_manager.batch_dir = "script/batch"
     config.batch_manager.save_log = true
     config.batch_manager.log_dir = "log/batch"
+
+    initializer "batch_manager.assets.precompile" do |app|
+      app.config.assets.precompile += %w(batch_manager/editor.js)
+    end
   end
 end
