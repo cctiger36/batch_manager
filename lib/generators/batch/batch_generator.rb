6c4356cc49e0362582962605f57be6f1683c9088
Weihu Chen <cctiger36@gmail.com>
2013-06-21 22:37:52 +0900

    fix the default batch template

--- lib/generators/batch/batch_generator.rb
+++ lib/generators/batch/batch_generator.rb
@@ -12,7 +12,7 @@ module Rails
       end
 
       def self.source_root
-        File.join(File.dirname(__FILE__), 'templates', (orm.to_s unless orm.class.eql?(String)) )
+        File.join(File.dirname(__FILE__), 'templates', (orm.to_s unless orm.class.eql?(String)))
       end
 
       def create_batch_file
