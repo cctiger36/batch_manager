587438f11832df016efaa2b747e892825c3927e4
Weihu Chen <cctiger36@gmail.com>
2014-02-28 17:07:20 +0900

    fix batch template

--- lib/generators/batch/templates/active_record/batch.rb
+++ lib/generators/batch/templates/active_record/batch.rb
@@ -3,13 +3,12 @@
 # =created_at:  <%= Time.now.strftime "%Y-%m-%d %H:%M:%S" %>
 # =times_limit: 1
 
-wet_run = (ARGV[0] == "wet" || @wet)
-
 ActiveRecord::Base.transaction do
 
   # Code at here will rollback when dry run.
 
-  if wet_run
+  # @wet will be true when -f option passed.
+  if @wet
     BatchManager.logger.info "Wet run completed!"
   else
     BatchManager.logger.warn "Rolling back."
